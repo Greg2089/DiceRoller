751930b422ccc139eab19b32a59dd04aea66e963
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2023-05-26 13:23:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/** Это действие позволяет пользователю бросить кости и просмотреть результат
+ * на экране.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Бросайте кости и обновляйте экран с результатом.
+     */
+    private fun rollDice() {
+        // Создайте первый объект Dice с 6 сторонами и бросьте его.
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Создайте второй объект Dice с 6 сторонами и бросьте его.
+        val diceTwo = Dice(6)
+        val diceRollTwo = diceTwo.roll()
+
+        // Обновите экран с помощью броска костей
+        val resultTextView = findViewById<TextView>(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+        // Обновите экран с помощью броска костей
+        val resultTextViewTwo = findViewById<TextView>(R.id.textView2)
+        resultTextViewTwo.text = diceRollTwo.toString()
+
+    }
+}
+
+/**
+ * Класс для создания объекта с фиксированным числом граней.
+ */
+class Dice(private val numSides: Int) {
+
+    /**
+     * Сделайте случайный бросок кубика и верните результат.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
